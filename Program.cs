9b20c1ad8033809a354d1ad008566e94a77a6d6e
Yantimir <mingazitdinov@mail.ru>
2022-11-09 20:18:48 +0400

    задача 56

--- Program.cs
+++ Program.cs
@@ -10,6 +10,90 @@
 // 9 5 3 2
 // 8 4 4 2
 
+// Console.Write("Введите количество строк массива: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] Fill2DArray()
+// {
+//   int[,] array = new int[rows, columns];
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       array[i, j] = new Random().Next(1, 21);
+//     }
+//   }
+//   return array;
+// }
+// void Print2DArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write($"{array[i, j]}\t");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+// void Sort2DArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       for (int k = 0; k < array.GetLength(1) - 1; k++)
+//       {
+//         if (array[i, k] < array[i, k + 1])
+//         {
+//           int temp = array[i, k + 1];
+//           array[i, k + 1] = array[i, k];
+//           array[i, k] = temp;
+//         }
+//       }
+//     }
+//   }
+// }
+
+// void PrintSort2DArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write($"{array[i, j]}\t");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+// int[,] massiv = Fill2DArray();
+// Print2DArray(massiv);
+// Console.WriteLine($"\nОтсортированный массив: ");
+// Sort2DArray(massiv);
+// PrintSort2DArray(massiv);
+
+
+
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
 Console.Write("Введите количество строк массива: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов массива: ");
@@ -22,7 +106,7 @@ int[,] Fill2DArray()
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      array[i, j] = new Random().Next(1, 21);
+      array[i, j] = new Random().Next(1, 51);
     }
   }
   return array;
@@ -38,38 +122,28 @@ void Print2DArray(int[,] array)
     Console.WriteLine();
   }
 }
-void Sort2DArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
-      {
-        if (array[i, k] < array[i, k + 1])
-        {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
-        }
-      }
-    }
-  }
-}
-
-void PrintSort2DArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write($"{array[i, j]}\t");
-    }
-    Console.WriteLine();
-  }
-}
 int[,] massiv = Fill2DArray();
 Print2DArray(massiv);
-Console.WriteLine($"\nОтсортированный массив: ");
-Sort2DArray(massiv);
-PrintSort2DArray(massiv);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(massiv, 0);
+for (int i = 1; i < massiv.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(massiv, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i, 0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i, j];
+  }
+  return sumLine;
+}
+Console.WriteLine();
+Console.WriteLine($"строкa с наименьшей суммой элементов - {minSumLine + 1}");
