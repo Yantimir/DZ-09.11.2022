cae2a410be702aca12604ee47ea4580ed305059b
Yantimir <mingazitdinov@mail.ru>
2022-11-09 20:29:54 +0400

    задача 62

--- Program.cs
+++ Program.cs
@@ -94,56 +94,100 @@
 // Программа считает сумму элементов в каждой строке 
 // и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-Console.Write("Введите количество строк массива: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов массива: ");
-int columns = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество строк массива: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
 
-int[,] Fill2DArray()
+// int[,] Fill2DArray()
+// {
+//   int[,] array = new int[rows, columns];
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       array[i, j] = new Random().Next(1, 51);
+//     }
+//   }
+//   return array;
+// }
+// void Print2DArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write($"{array[i, j]}\t");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+// int[,] massiv = Fill2DArray();
+// Print2DArray(massiv);
+
+// int minSumLine = 0;
+// int sumLine = SumLineElements(massiv, 0);
+// for (int i = 1; i < massiv.GetLength(0); i++)
+// {
+//   int tempSumLine = SumLineElements(massiv, i);
+//   if (sumLine > tempSumLine)
+//   {
+//     sumLine = tempSumLine;
+//     minSumLine = i;
+//   }
+// }
+// int SumLineElements(int[,] array, int i)
+// {
+//   int sumLine = array[i, 0];
+//   for (int j = 1; j < array.GetLength(1); j++)
+//   {
+//     sumLine += array[i, j];
+//   }
+//   return sumLine;
+// }
+// Console.WriteLine();
+// Console.WriteLine($"строкa с наименьшей суммой элементов - {minSumLine + 1}");
+
+
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int size = 4;
+int[,] array = new int[size, size];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= array.GetLength(0) * array.GetLength(1))
 {
-  int[,] array = new int[rows, columns];
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(1, 51);
-    }
-  }
-  return array;
+  array[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= array.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > array.GetLength(1) - 1)
+    j--;
+  else
+    i--;
 }
-void Print2DArray(int[,] array)
+void PrintArray(int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      Console.Write($"{array[i, j]}\t");
+      if (array[i, j] / 10 <= 0)
+        Console.Write($" {array[i, j]} ");
+      else Console.Write($"{array[i, j]} ");
     }
     Console.WriteLine();
   }
 }
-int[,] massiv = Fill2DArray();
-Print2DArray(massiv);
-
-int minSumLine = 0;
-int sumLine = SumLineElements(massiv, 0);
-for (int i = 1; i < massiv.GetLength(0); i++)
-{
-  int tempSumLine = SumLineElements(massiv, i);
-  if (sumLine > tempSumLine)
-  {
-    sumLine = tempSumLine;
-    minSumLine = i;
-  }
-}
-int SumLineElements(int[,] array, int i)
-{
-  int sumLine = array[i, 0];
-  for (int j = 1; j < array.GetLength(1); j++)
-  {
-    sumLine += array[i, j];
-  }
-  return sumLine;
-}
-Console.WriteLine();
-Console.WriteLine($"строкa с наименьшей суммой элементов - {minSumLine + 1}");
+PrintArray(array);
